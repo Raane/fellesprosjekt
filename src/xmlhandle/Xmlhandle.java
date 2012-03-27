09f1b8e0acede9ae55499a35ba3aa9582b638d31
Joakim Hommeland <sirjoeyh@gmail.com>
2012-03-27 12:50:48 +0200

    Fininshed implementation of the server-side database framework

--- src/xmlhandle/Xmlhandle.java
+++ src/xmlhandle/Xmlhandle.java
@@ -3,11 +3,10 @@ package xmlhandle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
-import java.text.DateFormat;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,8 +16,6 @@ import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
 import dbhandle.Event;
-import dbhandle.Meeting;
-import dbhandle.MeetingRoom;
 import dbhandle.MessageAction;
 import dbhandle.Status;
 import dbhandle.User;
@@ -34,19 +31,17 @@ public class Xmlhandle {
 	}
 	
 	//Receiver methods
-	public boolean performMessageInstructions(Document xml) throws NumberFormatException, ParseException, SQLException{
+	public void performMessageInstructions(Document xml) throws NumberFormatException, ParseException, SQLException{
 		
 		Element root = xml.getRootElement();
-		int ownerID;
+		String ownerUsername = null;
 		
 		Element ownerElement = root.element("owner");
 		if (ownerElement != null) {
-		ownerID = Integer.valueOf(ownerElement.attributeValue("owner_ID"));
-		} else {
-			ownerID = -1;
-		}
+		ownerUsername = ownerElement.attributeValue("owner_username");
+		} 
 		
-		Xmlaction actionToPerform = new Xmlaction(ownerID);
+		Xmlaction actionToPerform = new Xmlaction(ownerUsername);
 		MessageAction action = MessageAction.valueOf(root.getName());
 		
 		if (action == MessageAction.LOGIN) {
@@ -57,7 +52,7 @@ public class Xmlhandle {
 			String username = loginCandidate.attributeValue("username");
 			String password = loginCandidate.attributeValue("password");
 						
-			return actionToPerform.loginSuccess(username, password);
+			actionToPerform.loginSuccess(username, password);
 			
 		} else if (action == MessageAction.LOGOUT) {
 			
@@ -65,13 +60,17 @@ public class Xmlhandle {
 			
 		} else if (action == MessageAction.CREATE_MEETING) {
 			
-			List<Integer> userIDList; Event newEvent; int meetingRoomID = -1;
+			List<Integer> userIDList; Event newEvent; int meetingRoomID = -1; String meetingName;
 			
 			//Finds all User objects
 			userIDList = XMLtoUserIDList(root);
 			
 			//Finds the event it needs
 			newEvent = XMLtoEvent(root);
+			
+			//Finds the meeting name
+			Element meetingElement = root.element("meeting");
+			meetingName = meetingElement.attributeValue("meeting_name");
 
 			
 			//Does the meeting have a booked room? -- Hope this returns null if it doesn't find the argument
@@ -82,19 +81,18 @@ public class Xmlhandle {
 			}
 			
 			//Perform the action
-			actionToPerform.createMeeting(userIDList, newEvent, meetingRoomID);
+			actionToPerform.createMeeting(userIDList, newEvent, meetingRoomID, meetingName);
 			
 		} else if (action == MessageAction.EDIT_MEETING) {
 			
-			Event eventToEdit; int meetingID;
+			Event eventChanges; int meetingID;
 			
-			eventToEdit = XMLtoEvent(root);
+			eventChanges = XMLtoEvent(root);
 			
-			Element idElement = root.element("meeting");
-			meetingID = Integer.valueOf(idElement.attributeValue("meeting_ID"));
+			Element meetingElement = root.element("meeting");
+			meetingID = Integer.valueOf(meetingElement.attributeValue("meeting_ID"));
 			
-			actionToPerform.editMeeting(eventToEdit, meetingID);
-			
+			actionToPerform.editMeeting(eventChanges, meetingID);
 			
 		} else if (action == MessageAction.CREATE_USER) {
 			
@@ -126,10 +124,7 @@ public class Xmlhandle {
 			
 		} else if (action == MessageAction.DELETE) {
 			
-		}
-		
-		System.out.println("End of method");
-		return false;
+		}		
 				
 	}	
 	
@@ -153,17 +148,24 @@ public class Xmlhandle {
 		return null;
 	}
 	
-	//This will run on the client side. This can probably user the User class?
-	public Document createAddNewMeetingRequest(List<User> userList, Event event, int meetingRoomID) {
+	//This will run on the client side. This can probably user the User class? -- tested and should be working
+	public Document createAddMeetingRequest(List<Integer> userList, Event event, int meetingRoomID, String meetingName, String username) {
 		
 		Document document = DocumentHelper.createDocument();
 		Element root = document.addElement(MessageAction.CREATE_MEETING.toString());
 		
+		//Adds the user_ID and the username of the owner
+		root.addElement("owner")
+		.addAttribute("owner_username",username);
+		
 		//The users to be added
-		for (User user : userList) {
+		for (int userID : userList) {
 			root.addElement("user")
-			.addAttribute("user_ID", String.valueOf(user.getUserID()));
+			.addAttribute("user_ID", String.valueOf(userID));
 		}
+		
+		root.addElement("meeting")
+		.addAttribute("meeting_name", meetingName);
 		
 		//The event that will be created for all the users
 		//TODO: Fix the problem with the dates
@@ -185,17 +187,24 @@ public class Xmlhandle {
 	}
 	
 	//TODO: Change it to only include the changes?
-	public Document createEditMeetingRequest(Event eventToEdit, int meetingID) {
+	public Document createEditMeetingRequest(Event eventChanges, int meetingID, String username) {
+		
+		//Here eventChanges includes the original events ID aswell as all the changes
 		
 		Document document = DocumentHelper.createDocument();
 		Element root = document.addElement(MessageAction.EDIT_MEETING.toString());
 		
+		//Adds the user_ID and the username of the owner
+		root.addElement("owner")
+		.addAttribute("owner_username", username);
+		
 		root.addElement("event")
-		.addAttribute("start", eventToEdit.getStart().toString())
-		.addAttribute("end", eventToEdit.getEnd().toString())
-		.addAttribute("location", eventToEdit.getLocation())
-		.addAttribute("description",eventToEdit.getDescription())
-		.addAttribute("status",eventToEdit.getStatus().toString());
+		.addAttribute("event_ID", String.valueOf(eventChanges.getEvent_ID()))
+		.addAttribute("start", eventChanges.getStart().toString())
+		.addAttribute("end", eventChanges.getEnd().toString())
+		.addAttribute("location", eventChanges.getLocation())
+		.addAttribute("description",eventChanges.getDescription())
+		.addAttribute("status",eventChanges.getStatus().toString());
 		
 		root.addElement("meeting")
 		.addAttribute("meeting_ID", String.valueOf(meetingID));
@@ -204,13 +213,16 @@ public class Xmlhandle {
 		
 	}
 	
-	public Document createAddUserRequest(User newUser) {
+	public Document createAddUserRequest(User newUser, String username) {
 		
 		Document document = DocumentHelper.createDocument();
 		Element root = document.addElement(MessageAction.CREATE_USER.toString());
 		
+		//Adds the user_ID and the username of the owner
+		root.addElement("owner")
+		.addAttribute("owner_username",username);
+		
 		root.addElement("user")
-		.addAttribute("user_ID", String.valueOf(-1))
 		.addAttribute("username", newUser.getUsername())
 		.addAttribute("password", newUser.getPassword())
 		.addAttribute("name", newUser.getName());
@@ -218,10 +230,14 @@ public class Xmlhandle {
 		return document;
 	}
 	
-	public Document createEditNameOfUserRequest(String newName) {
+	public Document createEditNameOfUserRequest(String newName, String username) {
 		
 		Document document = DocumentHelper.createDocument();
 		Element root = document.addElement(MessageAction.EDIT_NAME_OF_USER.toString());
+		
+		//Adds the user_ID and the username of the owner
+		root.addElement("owner")
+		.addAttribute("owner_username",username);
 		
 		root.addElement("change_name")
 		.addAttribute("new_name", newName);
@@ -229,13 +245,13 @@ public class Xmlhandle {
 		return document;
 	}
 	
-	public Document createEditUserPasswordRequest(int ownerID, String oldPassword, String newPassword) {
+	public Document createEditUserPasswordRequest(String oldPassword, String newPassword, String username) {
 		
 		Document document = DocumentHelper.createDocument();
 		Element root = document.addElement(MessageAction.EDIT_USER_PASSWORD.toString());
 		
 		root.addElement("owner")
-		.addAttribute("owner_ID", String.valueOf(ownerID));
+		.addAttribute("owner_username", username);
 		
 		root.addElement("change_password")
 		.addAttribute("old_password", oldPassword)
@@ -244,52 +260,54 @@ public class Xmlhandle {
 		return document;
 	}
 	
-	public Document createEditEventRequest(Event eventToEdit) {
+	public Document createEditEventRequest(Event eventChanges, String username) {
 		
 		Document document = DocumentHelper.createDocument();
-		Element root = document.addElement(MessageAction.EDIT_MEETING.toString());
+		Element root = document.addElement(MessageAction.EDIT_EVENT.toString());
+		
+		//Adds the user_ID and the username of the owner
+		root.addElement("owner")
+		.addAttribute("owner_username",username);
 		
 		root.addElement("event")
-		.addAttribute("start", eventToEdit.getStart().toString())
-		.addAttribute("end", eventToEdit.getEnd().toString())
-		.addAttribute("location", eventToEdit.getLocation())
-		.addAttribute("description",eventToEdit.getDescription())
-		.addAttribute("status",eventToEdit.getStatus().toString());
+		.addAttribute("event_ID", String.valueOf(eventChanges.getEvent_ID()))
+		.addAttribute("start", eventChanges.getStart().toString())
+		.addAttribute("end", eventChanges.getEnd().toString())
+		.addAttribute("location", eventChanges.getLocation())
+		.addAttribute("description",eventChanges.getDescription())
+		.addAttribute("status",eventChanges.getStatus().toString());
 		
 		return document;
 	}
 	
 	//Any other methods needed?
 	
-	
-	
 	//Helper methods
-	private Date StringToDate(String string) throws ParseException {
-		  DateFormat formatter; 
-		  Date date; 
-		  formatter = new SimpleDateFormat("dd-MM-yy-HH-mm-ss");
-		  date = (Date)formatter.parse(string);  
-		  return date;
-		 
+	private Timestamp StringToDate(String string) throws ParseException {
+		
+		java.util.Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(string);
+		Timestamp timestamp = new Timestamp(date.getTime());
+		
+		return timestamp;
 		}  
 	
 	private Event XMLtoEvent(Element root) throws NumberFormatException, ParseException {
 		Element eventElement = root.element("event");
-		Date start = StringToDate(eventElement.attributeValue("start"));
-		Date end = StringToDate(eventElement.attributeValue("end"));
+		int userID = Integer.valueOf(eventElement.attributeValue("event_ID"));
+		Timestamp start = StringToDate(eventElement.attributeValue("start"));
+		Timestamp end = StringToDate(eventElement.attributeValue("end"));
 		String location = eventElement.attributeValue("location");
 		String description = eventElement.attributeValue("description");
 		Status status = Status.valueOf(eventElement.attributeValue("status"));
-		return new Event(start,end,location,description,status);
+		return new Event(userID,start,end,location,description,status);
 	}
 	
 	private User XMLtoUser(Element root) {
 		Element userElement = root.element("user");
-		String user_ID = userElement.attributeValue("user_ID");
         String username = userElement.attributeValue("username");
         String password = userElement.attributeValue("password");
         String name = userElement.attributeValue("name");
-        return new User(Integer.valueOf(user_ID),username,password,name);
+        return new User(username,password,name);
 	}
 	
 	private List<Integer> XMLtoUserIDList(Element root) {
@@ -317,13 +335,13 @@ public class Xmlhandle {
         }
 		return userList;
 	}
-	public static int extractUserID(String xml) throws DocumentException {
+	public static String extractUsername(String xml) throws DocumentException {
 		
 		Document document = stringToXML(xml);
 		Element root = document.getRootElement();
 		
 		Element owner = root.element("owner");
-		return Integer.valueOf(owner.attributeValue("owner_ID"));	
+		return null;	
 	}
 	
 	public void addListener(ActionListener listener) {

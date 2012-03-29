dd82d09ac78a35931013d730f68e693529435a38
Hakon Hesselberg <hakon.hesselberg@gmail.com>
2012-03-29 16:37:12 +0200

    Fancy shit, if you know what I mean

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,10 @@
 package gui;
 
-public class Main {
+import java.awt.Color;
 
+public class Main {
+	public static final Color focusColor = new Color(152,251,152);
+	
     public static void main(String[] args) {
         GUI gui = new GUI();
         gui.setVisible(true);
